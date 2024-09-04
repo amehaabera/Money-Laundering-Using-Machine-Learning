38749d2eebb3076f02bacc560ace4fc708996df3
Ameha Kidane <70386043+amehaabera@users.noreply.github.com>
2024-09-04 15:26:06 +0300

    app.py

--- app.py
+++ app.py
@@ -1,72 +1,101 @@
-import streamlit as st
-import pickle
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import streamlit as st
+import joblib
+import warnings
+import altair as alt
+import sklearn
+import base64
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
-# load the model and label encoder
-model = pickle.load(open('xgb_model.pkl', 'rb'))
+warnings.filterwarnings('ignore')
 
-# define function to preprocess user input
-def preprocess_input(payment, amount, oldbalanceOrg, newbalanceOrg, oldbalanceDest, newbalanceDest):
-    # create dataframe with user input
-    input_df = pd.DataFrame({'type': [payment], 
-                             'amount': [amount], 
-                             'oldbalanceOrg': [oldbalanceOrg], 
-                             'newbalanceOrg': [newbalanceOrg], 
-                             'oldbalanceDest': [oldbalanceDest], 
-                             'newbalanceDest': [newbalanceDest]})
-  
-    # return preprocessed input
-    return input_df
+# Load the trained model from the file
+model_path = r"C:\Users\User\.ipython\profile_default\my_new_model.pkl"
+model = joblib.load(model_path)  
+# Load the model using joblib
 
-# define Streamlit app
-def app():
-    # set app title
-    st.title('Money Laundering Detector')
-    
-    # add sidebar to select transaction type
-    types = st.sidebar.subheader("""
-                 Enter Type of Transfer Made:\n\n\n\n
-                 0 for 'CASH_IN' Transaction\n 
-                 1 for 'CaASH_OUT' Transaction\n 
-                 2 for 'DEBIT' Transaction\n
-                 3 for 'PAYMENT' Transaction\n  
-                 4 for 'TRANSFER' Transaction\n""")
-    types = st.sidebar.selectbox("",(0,1,2,3,4))
-    x = ''
-    if types == 0:
-        x = 'CASH_IN'
-    if types == 1:
-        x = 'CASH_OUT'
-    if types == 2:
-        x = 'DEBIT'
-    if types == 3:
-        x = 'PAYMENT'
-    if types == 4:
-        x =  'TRANSFER'
 
-    # define input fields
-    payment = x
-    amount = st.number_input('Amount')
-    oldbalanceOrg = st.number_input('Old Balance (Origin)')
-    newbalanceOrg = st.number_input('New Balance (Origin)')
-    oldbalanceDest = st.number_input('Old Balance (Destination)')
-    newbalanceDest = st.number_input('New Balance (Destination)')
+ 
+@st.cache  # Use st.cache instead of st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-    # preprocess user input
-    input_data = preprocess_input(payment, amount, oldbalanceOrg, newbalanceOrg, oldbalanceDest, newbalanceDest)
-    
-    # make prediction
-    prediction = model.predict(input_data)
-    
-    # display result
+img = get_img_as_base64("image.jpg")
+
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+    background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+    background-size: 280%;
+    background-position: top left;
+    background-repeat: no-repeat;
+    background-attachment: local;
+}}
+
+[data-testid="stHeader"] {{
+    background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+    right: 2rem;
+}}
+</style>
+"""
+
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
+def fraud_detection(input_data):
+    # Define feature names
+    feature_names = ['type', 'amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest']
+
+    # Convert input_data to a dictionary with feature names
+    input_dict = dict(zip(feature_names, input_data))
+
+    # Create a DataFrame with a single row using input_dict
+    input_df = pd.DataFrame(input_dict, index=[0])
+
+    # Make a prediction
+    prediction = model.predict(input_df)
+
     if prediction[0] == 0:
-        st.write('The Person is Fraud')
+        return 'Not a fraudulent transaction'
     else:
-        st.write('The Person Not Fraud')
+        return 'Fraudulent transaction'
 
-    # display input data
-    st.write('Input Data:')
-    st.write(input_data)
+def main():
+    st.title('**Money Laundering Transaction Detection System!!**')
+
+    options = ['PAYMENT', 'TRANSFER', 'CASH_OUT', 'DEBIT', 'CASH_IN']
+    values = [2, 4, 1, 5, 3]
+
+    # Create a selectbox for transaction type
+    type = st.selectbox('Select the Type of Transaction', options)
+
+    # Use a dictionary to map the selected transaction type to its corresponding value
+    type_to_value = dict(zip(options, values))
+    selected_value = type_to_value[type]
+
+    amount = st.text_input('Enter the Total Amount of Transaction')
+    oldbalanceOrg = st.text_input('Enter The old balance on the origin account before the transaction')
+    newbalanceOrig = st.text_input('Enter The new balance on the origin account after the transaction')
+    oldbalanceDest = st.text_input('Enter The old balance on the destination account before the transaction')
+    newbalanceDest = st.text_input('Enter The new balance on the destination account after the transaction')
+
+    prediction = ''
+
+    if st.button('Predict'):
+        input_data = [selected_value, float(amount), float(oldbalanceOrg), float(newbalanceOrig), float(oldbalanceDest), float(newbalanceDest)]
+        prediction = fraud_detection(input_data)
+        st.success(prediction)
 
 if __name__ == '__main__':
-    app()
+    main()
