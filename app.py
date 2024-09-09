1b20ad6290ae472cb0972df54a50c37217da44f2
Ameha Kidane <70386043+amehaabera@users.noreply.github.com>
2024-09-09 15:47:08 +0300

    app.py

--- app.py
+++ app.py
@@ -44,68 +44,49 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 
-# define function to preprocess user input
-def preprocess_input(payment, amount, oldbalanceOrg, newbalanceOrg, oldbalanceDest, newbalanceDest):
-    # create dataframe with user input
-    input_df = pd.DataFrame({'type': [payment], 
-                             'amount': [amount], 
-                             'oldbalanceOrg': [oldbalanceOrg], 
-                             'newbalanceOrg': [newbalanceOrg], 
-                             'oldbalanceDest': [oldbalanceDest], 
-                             'newbalanceDest': [newbalanceDest]})
-  
-    # return preprocessed input
-    return input_df
+def fraud_detection(input_data):
+    # Define feature names
+    feature_names = ['type', 'amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest']
 
-# define Streamlit app
-def app():
-    # set app title
-    st.title('Money Laundering Detector')
-    
-    # add sidebar to select transaction type
-    types = st.sidebar.subheader("""
-                 Enter Type of Transfer Made:\n\n\n\n
-                 0 for 'CASH_IN' Transaction\n 
-                 1 for 'CaASH_OUT' Transaction\n 
-                 2 for 'DEBIT' Transaction\n
-                 3 for 'PAYMENT' Transaction\n  
-                 4 for 'TRANSFER' Transaction\n""")
-    types = st.sidebar.selectbox("",(0,1,2,3,4))
-    x = ''
-    if types == 0:
-        x = 'CASH_IN'
-    if types == 1:
-        x = 'CASH_OUT'
-    if types == 2:
-        x = 'DEBIT'
-    if types == 3:
-        x = 'PAYMENT'
-    if types == 4:
-        x =  'TRANSFER'
+    # Convert input_data to a dictionary with feature names
+    input_dict = dict(zip(feature_names, input_data))
 
-    # define input fields
-    payment = x
-    amount = st.number_input('Amount')
-    oldbalanceOrg = st.number_input('Old Balance (Origin)')
-    newbalanceOrg = st.number_input('New Balance (Origin)')
-    oldbalanceDest = st.number_input('Old Balance (Destination)')
-    newbalanceDest = st.number_input('New Balance (Destination)')
+    # Create a DataFrame with a single row using input_dict
+    input_df = pd.DataFrame(input_dict, index=[0])
 
-    # preprocess user input
-    input_data = preprocess_input(payment, amount, oldbalanceOrg, newbalanceOrg, oldbalanceDest, newbalanceDest)
-    
-    # make prediction
-    prediction = model_path.predict(input_data)
-    
-    # display result
+    # Make a prediction
+    prediction = model.predict(input_df)
+
     if prediction[0] == 0:
-        st.write('The Person is Fraud')
+        return 'Not a fraudulent transaction'
     else:
-        st.write('The Person Not Fraud')
+        return 'Fraudulent transaction'
 
-    # display input data
-    st.write('Input Data:')
-    st.write(input_data)
+def main():
+    st.title('**Money Laundering Transaction Detection System!!**')
+
+    options = ['PAYMENT', 'TRANSFER', 'CASH_OUT', 'DEBIT', 'CASH_IN']
+    values = [2, 4, 1, 5, 3]
+
+    # Create a selectbox for transaction type
+    type = st.selectbox('Select the Type of Transaction', options)
+
+    # Use a dictionary to map the selected transaction type to its corresponding value
+    type_to_value = dict(zip(options, values))
+    selected_value = type_to_value[type]
+
+    amount = st.text_input('Enter the Total Amount of Transaction')
+    oldbalanceOrg = st.text_input('Enter The old balance on the origin account before the transaction')
+    newbalanceOrig = st.text_input('Enter The new balance on the origin account after the transaction')
+    oldbalanceDest = st.text_input('Enter The old balance on the destination account before the transaction')
+    newbalanceDest = st.text_input('Enter The new balance on the destination account after the transaction')
+
+    prediction = ''
+
+    if st.button('Predict'):
+        input_data = [selected_value, float(amount), float(oldbalanceOrg), float(newbalanceOrig), float(oldbalanceDest), float(newbalanceDest)]
+        prediction = fraud_detection(input_data)
+        st.success(prediction)
 
 if __name__ == '__main__':
-    app()
+    main()
