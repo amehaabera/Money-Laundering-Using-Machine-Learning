6fe3a8c0638d0da44232d149bb0224315dd6dc01
Ameha Kidane <70386043+amehaabera@users.noreply.github.com>
2024-09-12 15:38:37 +0300

    app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def main():
     selection = st.sidebar.radio("Go to", ["Home", "App", "About"])
 
     if selection == "Home":
-        st.title("Welcome to the Fraud Detection System")
+        st.title("Welcome to the Anti-money laundering (AML) transaction monitoring software System")
         st.write("""
         This application uses machine learning models to predict the likelihood of a transaction being fraudulent.
         You can enter the transaction details and the system will predict if the transaction is **Normal** or **Fraudulent**.
